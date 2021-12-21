425a47cb29d0e4034ea691e33f0c46efad75afb2
Alexwebcreator <Sanek72Tob@mail.ru>
2021-12-21 20:21:47 +0500

    Выполнил дополнительное задание урока 15

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const DomElement = function (selector, height, width, bg, fontSize, text) {
   this.text = text;
   this.render = function () {
     let elem;
-    let elementName = prompt("Назови элемент, начни ввод с . или # ");
+    let elementName = this.selector;
     let dot = elementName[0] == "." ? elementName.substr(1) : elementName;
     let sharp = elementName[0] == "#" ? elementName.substr(1) : elementName;
     switch (true) {
@@ -26,20 +26,20 @@ const DomElement = function (selector, height, width, bg, fontSize, text) {
       default:
         console.log("Ошибка");
     }
-    function isNumber(n) {
-      return !isNaN(parseFloat(n)) && isFinite(n);
-    }
-    do {
-      this.height = prompt("Высота элемента");
-    } while (!isNumber(this.height));
-    do {
-      this.width = prompt("Ширина элемента");
-    } while (!isNumber(this.width));
-    let elemBg = prompt("Цвет фона на английском или #Hex color)");
-    do {
-      this.fontSize = prompt("Размер текста");
-    } while (!isNumber(this.fontSize));
-    let elemText = prompt("Внутренний текст элемента");
+    //function isNumber(n) {
+      //return !isNaN(parseFloat(n)) && //isFinite(n);
+    //}
+    //do {
+      //this.height = prompt("Высота элемента");
+    //} while (!isNumber(this.height));
+    //do {
+      //this.width = prompt("Ширина элемента");
+    //} while (!isNumber(this.width));
+    //let elemBg = prompt("Цвет фона на английском или #Hex color)");
+    //do {
+      //this.fontSize = prompt("Размер текста");
+    //} while (!isNumber(this.fontSize));
+    //let elemText = prompt("Внутренний текст элемента");
     
     if (elem) {
       elem.style.cssText =
@@ -50,11 +50,11 @@ const DomElement = function (selector, height, width, bg, fontSize, text) {
         this.width +
         "px; " +
         "background:" +
-        elemBg +
+        this.bg +
         ";" +
         "font-size:" +
         this.fontSize + "px; ";
-      elem.textContent = elemText;
+      elem.textContent = this.text;
       document.body.append(elem);
     } else {
       alert("Начните ввод с (.) или (#)");
@@ -65,6 +65,48 @@ const DomElement = function (selector, height, width, bg, fontSize, text) {
 
 };
 
+//Обязательное задание
+//const obj = new DomElement();
+//obj.render();
 
-const obj = new DomElement();
-obj.render();
+
+const square = new DomElement(".square", "100", "100", "green");
+let squareHTML;
+let x = 0;
+let y = 0;
+
+const keyMove = (event) => {
+  switch (true) {
+    case event.code == "ArrowDown":
+      y += 10;
+      break;
+    case event.code == "ArrowUp":
+      if (y >= 10) {
+        y -= 10;
+      }
+      break;
+    case event.code == "ArrowRight":
+      const clientWidth = document.documentElement.clientWidth;
+      if(x < clientWidth - 120) {
+        x += 10;
+      }
+      break;
+    case event.code == "ArrowLeft":
+      if (x >= 10) {
+        x -= 10;
+      }
+      break;
+  }
+
+  squareHTML.style.transform = "translate(" + x + "px, " + y + "px)";
+
+};
+
+document.addEventListener("DOMContentLoaded", square.render());
+
+squareHTML = document.querySelector(".square");
+
+square.position = "absolute";
+squareHTML.style.position = square.position;
+
+document.addEventListener("keydown", keyMove);
